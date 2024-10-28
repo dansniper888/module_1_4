3157c16348c4bc1d31b96a4e032c833af7414e20
dansniper888 <dansniper888@gmail.com>
2024-10-28 22:38:16 +0700

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,5 @@
+quantity_dz = 12
+quantity_ch = 1.5
+name_course = 'Pethon'
+sn = quantity_ch / quantity_dz
+print('Курс:', name_course, 'Всего задач:', quantity_dz, 'Затрачено часов:', quantity_ch, 'Среднее время выполнения:', sn)
